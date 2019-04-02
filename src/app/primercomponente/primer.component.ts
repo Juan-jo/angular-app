ee6f7fe0c48fc00fb7dae4fd018a344d5f25ea1e
Joanjo <mtz230396@gmail.com>
2019-04-02 00:28:24 -0600

    Agrega primer componente

---
+++ src/app/primercomponente/primer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: '<h1>Hello World</h1>',
+  styles: ['h1 {background: #000; color: #fff;}']
+})
+
+export class HelloWorld {
+
+}
